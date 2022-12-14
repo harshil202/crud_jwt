5c5349869960c8084057cd4efa98ed4420f6b09e
unknown <administrator@NETIZENS.LOCAL>
2022-12-14 10:38:23 +0530

    add gitignore

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ db.sequelize.sync().then(() => {
 
 app.use("/api", userRoutes)
 
+//start the server
 app.listen(3000, () =>{
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port 3000") 
 })
